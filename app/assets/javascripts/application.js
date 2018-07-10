2fe9c56b5002ed50c26cacfa70534b976161039b
Larry Miles <lmiles@lundinoir.com>
2018-07-10 09:34:36 +0100

    guilty plea character count update

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,6 +7,40 @@ if (window.console && window.console.info) {
 
 $(document).ready(function () {
 
-  // add code here
+    /* *************** */
+    /* *************** */
+    /* *************** */
+    /* *************** */
+    /* CHARACTER COUNT */
+    var text_max = 50; // This the maximum character count
+    var text_spread = 10; // This calculates when to change the text colour to red
+
+    $('#character_count').html('Character count: 0 /' + text_max);
+
+    $('#guilty-more-detail').keyup(function() {
+        var text_length = $('#guilty-more-detail').val().length;
+
+        if (text_length > (text_max/2)) {
+            $('#character_count').css('display','block');
+            $('#character_count').html('You have ' + (text_max-text_length) + ' characters remaining');
+        } else {
+            $('#character_count').css('display','none');
+        }
+
+        if (text_length > (text_max-text_spread)) {
+            $('#character_count').css('color','#B10E1E');
+        } else {
+            $('#character_count').css('color','#6F777B');
+        }
+
+        if (text_length > text_max) {
+            $('#character_count').addClass('govuk-!-font-weight-bold');
+            $('#character_count').html('You have ' + (text_max-text_length)*-1 + ' characters too many');
+            $('#guilty-more-detail').addClass('character_count_highlight');
+        } else {
+            $('#character_count').removeClass('govuk-!-font-weight-bold');
+            $('#guilty-more-detail').removeClass('character_count_highlight');
+        }
+    });        
 
 })
